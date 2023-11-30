428710efe842a971ee9302d6a4e120255f2bd659
Grant Forrester <grant.forrester@adarma.com>
2023-11-30 00:55:57 +0000

    test: check we're on the same version of api

--- tests/openapi.test.js
+++ tests/openapi.test.js
@@ -1,10 +1,14 @@
 const { spec } = require('pactum')
 const config = require('./config')
+const fs = require('fs')
+
+openapi = fs.readFileSync('openapi.yml')
 
 describe('openapi', () => {
     it('should return expected yaml', async () => {
         await spec()
             .get(`${config.server}/openapi.yml`)
             .expectStatus(200)
+            .expectBody(openapi.toString())
     })
 })
